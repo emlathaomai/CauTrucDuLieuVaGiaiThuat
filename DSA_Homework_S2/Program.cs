0995c40a1bcd884ad8f70cfa07dd59aa0df4f0fe
vtd <2500115772@vanlangsaigon.edu.vn>
2026-03-16 09:39:24 +0700

    upload homework s2

---
+++ DSA_Homework_S2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+    class Program
+{
+    static int TinhTong(int n)
+    {
+        
+        if (n ==1 ) return 1;
+
+        return n + TinhTong(n-1);
+    }
+
+
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Nhap n de tinh tong:");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Tong S({n}) = {TinhTong(n)}");
+    }
+    }
