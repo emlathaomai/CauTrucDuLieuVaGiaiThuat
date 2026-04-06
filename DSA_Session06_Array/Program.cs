26bf744cbd192184c7a33ec3345929949d122621
vtd <2500115772@vanlangsaigon.edu.vn>
2026-04-06 08:52:38 +0700

    Khoi tao cau truc Buoi 6.02

---
+++ DSA_Session06_Array/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DSA_Session06_Array;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ DSA_Session06_Basics/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DSA_Session06_Basics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
